b7b0b7276575b014d135d38fd15c4b24c7548e60
cipher <sarthakshirbhate081@gmail.com>
2024-04-06 23:17:08 +0530

    its a dayi

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -1,13 +1,15 @@
+
 package com.driver;
 
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name, 4, 2, 6, isManual, "race", 4);
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = getCurrentSpeed() + rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -20,11 +22,37 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            changeGear(1);
+            setCurrentSpeed(0);
+//            return;
+        } else if (newSpeed > 250){
+//            super.changeSpeed(newSpeed, super.getCurrentDirection());
+            changeGear(6);
+            setCurrentSpeed(newSpeed);
+//            return;
+        } else if (newSpeed > 200){
+//            super.changeSpeed(newSpeed, super.getCurrentDirection());
+            changeGear(5);
+            setCurrentSpeed(newSpeed);
+            return;
+        } else if (newSpeed > 150){
+//            super.changeSpeed(newSpeed, super.getCurrentDirection());
+            changeGear(4);
+            setCurrentSpeed(newSpeed);
+        } else if (newSpeed > 100){
+//            super.changeSpeed(newSpeed, super.getCurrentDirection());
+            changeGear(3);
+            setCurrentSpeed(newSpeed);
         }
-        //for all other cases, change the gear accordingly
-
-        if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
+        else if (newSpeed > 50){
+            changeGear(2);
+            setCurrentSpeed(newSpeed);
+//            super.changeSpeed(newSpeed, super.getCurrentDirection());
+        } else if(newSpeed > 0) {
+//            super.changeSpeed(newSpeed, super.getCurrentDirection());
+            changeGear(1);
+            setCurrentSpeed(newSpeed);
         }
     }
 }
